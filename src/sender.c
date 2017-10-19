c9211163ee38f76d05a21dcfbd840f36549ca0a3
nbyouri <youri.mout@gmail.com>
2017-10-19 17:15:47 +0200

    Functionalise send_data.

--- src/sender.c
+++ src/sender.c
@@ -50,13 +50,89 @@ get_payload(char **buf, FILE *f, char *data, size_t offset, size_t length)
     }
 
     if (data != NULL) {
-        /* Update length if smaller than 512 XXX */
         memcpy(&tmp, data + offset, length);
     }
 
     memcpy(*buf, tmp, length);
     return 0;
 }
+
+/*
+ *
+ * Build initial window
+ *
+ */
+static void
+make_window(pkt_t *sliding_window[], FILE *f, char *data, size_t *nb_pkt, size_t total_pkt_to_send,
+	    size_t *data_offset, size_t *left_to_copy,
+	    uint8_t *seqnum, uint8_t *window)
+{
+    struct timeval current_time = {0};
+    for (; *nb_pkt < MAX_WINDOW_SIZE; (*nb_pkt)++) {
+	/* stop if the window size is bigger than file size */
+        if (*nb_pkt >= total_pkt_to_send)
+            break;
+
+        sliding_window[*nb_pkt] = pkt_new();
+        char *buf;
+
+        pkt_set_type(sliding_window[*nb_pkt], PTYPE_DATA);
+        pkt_set_tr(sliding_window[*nb_pkt], 0);
+        pkt_set_seqnum(sliding_window[*nb_pkt], *seqnum);
+        pkt_set_window(sliding_window[*nb_pkt], *window);
+        update_time(&current_time);
+        pkt_set_timestamp(sliding_window[*nb_pkt], pack_timestamp(current_time));
+
+        size_t length = MAX_PAYLOAD_SIZE;
+        if (*left_to_copy < MAX_PAYLOAD_SIZE)
+            length = *left_to_copy;
+        buf = malloc(length);
+	memset(buf, '\0', length);
+        get_payload(&buf, f, data, *data_offset, length);
+        pkt_set_payload(sliding_window[*nb_pkt], buf, length);
+        pkt_set_crc1(sliding_window[*nb_pkt], pkt_gen_crc1(sliding_window[*nb_pkt]));
+        pkt_set_crc2(sliding_window[*nb_pkt], pkt_gen_crc2(sliding_window[*nb_pkt]));
+
+        /* Bookkeeping */
+        *data_offset += length;
+        *left_to_copy -= length;
+        if (*seqnum + 1 >= MAX_SEQNUM)
+            *seqnum = 0;
+        else (*seqnum)++;
+
+        free(buf);
+        buf = NULL;
+    }
+}
+
+/*
+ *
+ * Send last packet with a length of zero to terminate the transfer.
+ *
+ */
+static void
+send_terminating_packet(int sfd, uint8_t seqnum) {
+    /* Finish by sending a length = 0 packet */
+    size_t len = sizeof(pkt_t);
+    char *buf = malloc(len);
+    pkt_t *end_pkt = pkt_new();
+    pkt_set_type(end_pkt, PTYPE_DATA);
+    pkt_set_seqnum(end_pkt, seqnum); 
+    if (pkt_encode(end_pkt, buf, &len) != PKT_OK) {
+	ERROR("Failed to encode end_pkt");
+	goto end;
+    }
+    if (send(sfd, buf, sizeof(pkt_t), 0) == -1) {
+	ERROR("Failed to send end_pkt");
+	goto end;	
+    }
+
+end:
+    pkt_del(end_pkt);
+    free(buf);
+    buf = NULL;   
+}
+
 
 /*
  * Main send loop
@@ -67,8 +143,7 @@ send_data(FILE *f, char *data, size_t total_len, int sfd)
     /* Start by finding out how many packets we need to send in total */
     size_t total_pkt_to_send = nb_pkt_in_buffer(total_len);
 
-    /* Start by building the first window */
-    /* We keep 16KB of data in memory always */
+    /* Protocol variables */
     uint8_t seqnum = 0;
     uint8_t window = 1;
     size_t  nb_pkt = 0;
@@ -78,45 +153,11 @@ send_data(FILE *f, char *data, size_t total_len, int sfd)
     size_t  cur_pkt = 0;
     int	    keep_sending = 1;
     pkt_t   *sliding_window[MAX_WINDOW_SIZE];
-    struct timeval current_time = {.tv_sec = 0, .tv_usec = 0};
 
-    /* XXX start by initializing */
-    /* XXX put this in a loop for files bigger than 16kb -> seqnum wrap around */
-    for (; nb_pkt < MAX_WINDOW_SIZE; nb_pkt++) {
-	/* stop if the window size is bigger than file size */
-        if (nb_pkt >= total_pkt_to_send)
-            break;
-
-        sliding_window[nb_pkt] = pkt_new();
-        char *buf;
-
-        pkt_set_type(sliding_window[nb_pkt], PTYPE_DATA);
-        pkt_set_tr(sliding_window[nb_pkt], 0);
-        pkt_set_seqnum(sliding_window[nb_pkt], seqnum);
-        pkt_set_window(sliding_window[nb_pkt], window);
-        update_time(&current_time);
-        pkt_set_timestamp(sliding_window[nb_pkt], pack_timestamp(current_time));
-
-        size_t length = MAX_PAYLOAD_SIZE;
-        if (left_to_copy < MAX_PAYLOAD_SIZE)
-            length = left_to_copy;
-        buf = malloc(length);
-	memset(buf, '\0', length);
-        get_payload(&buf, f, data, data_offset, length);
-        pkt_set_payload(sliding_window[nb_pkt], buf, length);
-        pkt_set_crc1(sliding_window[nb_pkt], pkt_gen_crc1(sliding_window[nb_pkt]));
-        pkt_set_crc2(sliding_window[nb_pkt], pkt_gen_crc2(sliding_window[nb_pkt]));
-
-        /* Bookkeeping */
-        data_offset += length;
-        left_to_copy -= length;
-        if (seqnum + 1 >= MAX_SEQNUM)
-            seqnum = 0;
-        else seqnum++;
-
-        free(buf);
-        buf = NULL;
-    }
+    /* Build the initial sliding window */
+    make_window(sliding_window, f, data, &nb_pkt,
+		total_pkt_to_send, &data_offset,
+		&left_to_copy, &seqnum, &window);
 
     /* Start encoding and sending the packets */
     size_t nb_pkt_win = nb_pkt; // XXX wrap if more than 1 window
@@ -144,90 +185,16 @@ send_data(FILE *f, char *data, size_t total_len, int sfd)
 	free(buf);
 	buf = NULL;
     }
+
     /* Cleanup */
     for (i = 0; i < nb_pkt; i++) {
 	pkt_del(sliding_window[i]);
     }
 
-    /* Finish by sending a length = 0 packet */
-    size_t len = sizeof(pkt_t);
-    char *buf = malloc(len);
-    pkt_t *end_pkt = pkt_new();
-    pkt_set_type(end_pkt, PTYPE_DATA);
-    pkt_set_seqnum(end_pkt, seqnum); 
-    if (pkt_encode(end_pkt, buf, &len) != PKT_OK) {
-	ERROR("Failed to encode end_pkt");
-	goto end;
-    }
-    if (send(sfd, buf, sizeof(pkt_t), 0) == -1) {
-	ERROR("Failed to send end_pkt");
-	goto end;	
-    }
-
-end:
-    pkt_del(end_pkt);
-    free(buf);
-    buf = NULL;   
+    send_terminating_packet(sfd, seqnum);
 }
 
 #if 0
-/*
- * Create send buffer containing everything to send for the current window
- * and the status.
- */
-static int
-make_window(pkt_t **,
-    size_t seqnum,
-    size_t *expected_seqnum,
-    size_t window,
-    size_t pkt_to_send,
-    FILE   *file,
-    size_t *pos_in_file,
-    size_t *payload_left_to_send)
-{
-    char    *payload_data = NULL;
-    size_t  payload_len = MAX_PAYLOAD_SIZE;
-    size_t  i = 0;
-
-    for (; i < pkt_to_send; i++) {
-        w[i] = calloc(1, sizeof(window_entry));
-        if (w[i] == NULL) {
-            return -1;
-        }
-        w[i]->pkt = pkt_new();
-
-        pkt_set_type(w[i]->pkt, PTYPE_DATA);
-        pkt_set_window(w[i]->pkt, window);
-        pkt_set_seqnum(w[i]->pkt, seqnum);
-        pkt_set_timestamp(w[i]->pkt, 0);
-
-        /* read file to get payload */
-        if (payload_len > *payload_left_to_send) {
-            payload_len = *payload_left_to_send;
-        }
-
-        if (payload_len == 0) {
-            payload_data = NULL;
-        } else {
-            get_payload(&payload_data, file, *pos_in_file, &payload_len);
-            *pos_in_file += payload_len;
-            *payload_left_to_send -= payload_len;
-        }
-
-        pkt_set_payload(w[i]->pkt, payload_data, payload_len);
-
-        w[i]->sent = 0;
-        w[i]->ack = 0;
-    }
-
-    *expected_seqnum += pkt_to_send;
-    if (*expected_seqnum >= MAX_SEQNUM) {
-        *expected_seqnum = *expected_seqnum - MAX_SEQNUM;
-    }
-
-    return 0;
-}
-
 /*
  * Loop through window entries and send them
  *
